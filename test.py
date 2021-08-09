e0867378b3b4b39cfd8b8476b428f32d0c24ae40
bsumner31 <bsumner.devops@gmail.com>
2021-08-09 14:59:13 -0500

    added unit tests, dockerignore and gitignore

---
+++ test.py
@@ -0,0 +1,23 @@
+from service import *
+
+
+
+def run_tests():
+  assert read_resp.status_code == 200, "Status code should be 200"
+
+  assert download_resp.status_code == 200, "Status code should be 200"
+
+  assert requests.get(url).content == b'Everything is working!', "Should return b'Everything is working!'"
+
+  assert download_resp.request.body == download_data, "Should return {action:download}"
+
+  assert read_resp.request.body == read_data, "Should return {action:read}"
+
+
+if __name__ == "__main__":
+  run_tests()
+  
+
+
+
+
